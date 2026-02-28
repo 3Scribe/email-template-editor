0c03075ba3343bcad49e0a9d9bd971aaf8dece4c
Eddie Gahan <gahan@users.noreply.github.com>
2026-02-28 15:20:22 +0000

    feat: frontend scaffold

---
+++ app/templates/page.tsx
@@ -0,0 +1,8 @@
+export default function TemplatesPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Templates</h1>
+      <p>Template listing will be added here.</p>
+    </main>
+  );
+}
